a662ee3fe3b3b874962da6a66e0919ba2f81fe8a
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-01-15 16:08:27 -0300

    added themes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
 import GlobalStyle from "./styles/globalStyles";
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './styles/theme'
 
 function App() {
+
+  const [isDarkTheme, setIsDarkTheme] = useState(true)
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") === 'true') {
+      setIsDarkTheme(true)
+    } else {
+      setIsDarkTheme(false)
+    }
+  }, []);
+
   return (
-    <GlobalStyle />
+
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+    background: '#fff',
+    border: '#eee',
+    mobileMenuBackground: '#eee',
+    menuItemHover: '#bbb',
+    text: '#000',
+    buttonThemeBackground: '#0A1929',
+    buttonThemeTextColor: '#fff'
+  }
+  
+  export const darkTheme = {
+    background: '#0A1929',
+    border: '#003060',
+    mobileMenuBackground: '#001E3C',
+    menuItemHover: '#4b59f7',
+    text: '#fff',
+    buttonThemeBackground: '#eee',
+    buttonThemeTextColor: '#000'
+  }
