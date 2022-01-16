934f9f884e42439ebbfee9718a7b7295eba1f270
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-01-16 14:47:07 -0300

    remove warning

--- src/pages/MoreCardsPage/index.js
+++ src/pages/MoreCardsPage/index.js
@@ -19,6 +19,7 @@ const MoreCardsPage = () => {
             .then((data) => {
                 setCards([...cards, ...data]);
             })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [params.race, currentPage]);
 
     useEffect(() => {
